8dc45d96a05934d24cc7039e8fbdbf5905c2e0b0
Gautier <Gautier@Gautier-PC>
2014-11-11 15:46:11 +0100

    correction of choices

--- Care4Care/C4CApplication/models/member.py
+++ Care4Care/C4CApplication/models/member.py
@@ -8,12 +8,12 @@ class Member(models.Model):
     picture = models.ImageField()
     birthday = models.DateField()
     TAG = (
-        (0,non_member),
-        (1,member),
-        (2,verified),
-        (3,volonteer),
-        (4, branch_off),
-        (5, bp_admin),
+        (0, 'non_member'),
+        (1, 'member'),
+        (2, 'verified'),
+        (3, 'volonteer'),
+        (4, 'branch_off'),
+        (5, 'bp_admin'),
     )
     
     tag = models.SmallIntegerField(choices = TAG)
@@ -25,10 +25,10 @@ class Member(models.Model):
     adresse = models.CharField(max_length=200)
     
     VISIBILITY = (
-        (0,anyone),
-        (1,verified),
-        (2,favorites),
-        (3,network),
+        (0, 'anyone'),
+        (1, 'verified'),
+        (2, 'favorites'),
+        (3, 'network'),
     )
     visibility = models.SmallIntegerField(choices = VISIBILITY)
     time_credit = models.BigIntegerField()
