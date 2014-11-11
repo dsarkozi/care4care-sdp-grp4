89b7a378e5979a03d770c3aa0b418932cf0ca7c5
Vineuvall <quentin.meyers@student.uclouvain.be>
2014-11-11 16:22:13 +0100

    quentin fait mumuse avec la data base

--- Care4Care/C4CApplication/models/branch.py
+++ Care4Care/C4CApplication/models/branch.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Branch(models.Model):
-    name = models.CharField(max_length, primary_key=True)
+    name = models.CharField(max_length=50, primary_key=True)
     town = models.CharField(max_length=200)
     branch_officer = models.EmailField()
     adress = models.CharField(max_length=200)
--- Care4Care/Care4Care/urls.py
+++ Care4Care/Care4Care/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'C4CApplication.views.test.home', name='home'),
 )
