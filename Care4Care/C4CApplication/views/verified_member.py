c422d6d785190883ad34c03788325b75829f03a1
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-11-16 12:08:44 +0100

    Un peu verified member

--- Care4Care/C4CApplication/views/verified_member.py
+++ Care4Care/C4CApplication/views/verified_member.py
@@ -6,11 +6,17 @@ class VerifiedMember(Member):
     This class represents a kind of Users called Verified Members
     """
 
+    # serieux c'est sense faire/renvoyer quoi ? 
     def see_job_details(self, job_id):
         #TODO
+        return False
 
     def get_job_list(self, show_offers):
-        #TODO
+        """ returns a QuerySet """
+        jobs = Job.objects.all(); 
+        #TODO a quoi sert show_offers ?
+        return jobs
 
     def accept_job(self, job_id, helped_one_email):
         #TODO
+        return False
