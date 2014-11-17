59e27494c1a0964d4c819da724695dc9e71dca7c
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-11-18 00:27:52 +0100

    volunteer_member

--- Care4Care/C4CApplication/views/volunteer_member.py
+++ Care4Care/C4CApplication/views/volunteer_member.py
@@ -6,11 +6,24 @@ class VolunteerMember(Member):
     This class represents a kind of Users called Volunteer Members
     """
 
-    def see_job_details(self, job_id, helped_one_email):
+    def is_job_visible(self, job, db_member):
+        """
+        :param job:
+        :param db_member:
+        :return: True if the job created by the member is visible
+        """
+
+        #This line have to change if we add the personal network
+        return job.visibility == Job.JOB_VISIBILITY['anyone']\
+               or job.visibility == Job.JOB_VISIBILITY['volunteer']\
+               or (job.visibility == Job.JOB_VISIBILITY['favorites']
+                   and db_member.is_favorite(self.db_member))
+               
+    """def see_job_details(self, job_id, helped_one_email):
         #TODO
 
     def get_job_list(self, show_offers):
         #TODO
 
     def accept_job(self, job_id, helped_one_email):
-        #TODO
+        #TODO"""
