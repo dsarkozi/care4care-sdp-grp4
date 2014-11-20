52c8fdf02a7f7675ec2b0d2b072aa1e8d62cfb84
David Sarkozi <david.sarkozi@student.uclouvain.be>
2014-11-21 00:07:35 +0100

    Dummy commit

--- Care4Care/C4CApplication/views/forms/LoginForm.py
+++ Care4Care/C4CApplication/views/forms/LoginForm.py
@@ -3,5 +3,5 @@ from django.forms.widgets import EmailInput, PasswordInput
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(required=True, widget=EmailInput(attrs={'class': 'login-input', 'placeholder': 'Email Address', 'autofocus': 'true'}))
-    password = forms.CharField(required=True, widget=PasswordInput(attrs={'class': 'login-input', 'placeholder': 'Password'}))
+    email = forms.EmailField(widget=EmailInput(attrs={'class': 'login-input', 'placeholder': 'Email Address', 'autofocus': 'true'}))
+    password = forms.CharField(widget=PasswordInput(attrs={'class': 'login-input', 'placeholder': 'Password'}))
