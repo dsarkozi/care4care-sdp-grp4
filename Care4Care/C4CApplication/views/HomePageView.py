b5070ded17aac0d387a91e8b4740913f933cf9a8
Gautier <Gautier@Gautier-PC>
2014-11-20 14:28:26 +0100

    get_visible_jobs_list instead of get_jobs_list

--- Care4Care/C4CApplication/views/HomePageView.py
+++ Care4Care/C4CApplication/views/HomePageView.py
@@ -5,11 +5,11 @@ class HomePageView(TemplateView):
     
     def demand_job_list(self):
         member = None # get_member TODO
-        return member.get_job_list(True)
+        return member.get_visible_jobs_list(True)
         
     def offert_job_list(self):
         member = None # get_member TODO
-        return member.get_job_list(False)
+        return member.get_visible_jobs_list(False)
     
     def click_on_job(self, job_number, job_creator_mail):
         member = None # get_member TODO
