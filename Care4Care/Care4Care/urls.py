bf38dd1eec5bf7394ef44e29b1138e7f348c70e7
David Sarkozi <david.sarkozi@student.uclouvain.be>
2014-11-21 00:51:42 +0100

    Added MyCare4Care url page

--- Care4Care/Care4Care/urls.py
+++ Care4Care/Care4Care/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from C4CApplication.views.HomePageView import HomePageView
+from C4CApplication.views.MyCare4CareView import MyCare4CareView
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +10,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomePageView.as_view(), name='home'),
+
+    url(r'^myc4c/', MyCare4CareView.as_view(), name='myc4c'),
 )
