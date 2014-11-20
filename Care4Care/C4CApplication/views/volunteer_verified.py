1072ea409d81f4e2bab3b73d8eca36885c7f374c
Gautier <Gautier@Gautier-PC>
2014-11-20 13:57:49 +0100

    renaming + correction of is_visible

--- Care4Care/C4CApplication/views/volunteer_verified.py
+++ Care4Care/C4CApplication/views/volunteer_verified.py
@@ -19,16 +19,17 @@ class VolunteerVerified(VolunteerMember, VerifiedMember):
                or (job.visibility & Job.JOB_VISIBILITY['verified']) == 1\
                or (job.visibility & Job.JOB_VISIBILITY['volunteer']) == 1\
                or (job.visibility & Job.JOB_VISIBILITY['favorites'] == 1
-                   and db_member.is_favorite(self.db_member))
+                   and db_member.is_favorite(self.db_member))\
+               or db_member == self.db_member
 
-    def see_job_details(self, job_id, helped_one_email):
+    def see_job_details(self, job_number, job_creator_mail):
         """
-        :param job_id: id of the job to return
-        :param helped_one_email: the email of the 'owner' of the job
-        :return: the instance of Job object represented by the 'job_id' if the user can see it
+        :param job_number: id of the job to return
+        :param job_creator_mail: the email of the 'owner' of the job
+        :return: the instance of Job object represented by the 'job_number' if the user can see it
                         otherwise, it returns None
         """
-        return self.see_job_details_base(job_id, helped_one_email, self.is_job_visible)
+        return self.see_job_details_base(job_number, job_creator_mail, self.is_job_visible)
 
     # herited by Non-Member from Member ?
     def get_job_list(self, show_offers):
@@ -39,13 +40,13 @@ class VolunteerVerified(VolunteerMember, VerifiedMember):
         """
         return self.get_job_list_base(show_offers, self.is_job_visible)
 
-    def accept_job(self, job_id, helped_one_email):
+    def accept_job(self, job_number, job_creator_mail):
         """
         Puts the member on the list of possible helpers for a pending job.
-        The helped one will be warned by email (this email is the parameter 'helped_one_email').
+        The helped one will be warned by email (this email is the parameter 'job_creator_mail').
 
-        :param job_id: the if of the job to accept
-        :param helped_one_email: the email of the 'owner' of the job
+        :param job_number: the if of the job to accept
+        :param job_creator_mail: the email of the 'owner' of the job
         :return: False if there was a problem and True otherwise
         """
-        return self.accept_job_base(job_id, helped_one_email, self.is_job_visible)
+        return self.accept_job_base(job_number, job_creator_mail, self.is_job_visible)
