cd2fcc2fb4ccfa2d7b6f81b84b128b4e6358677a
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-11-24 22:05:56 +0100

    post CJDF (TODO)

--- Care4Care/C4CApplication/views/forms/ConfirmJobDoneForm.py
+++ Care4Care/C4CApplication/views/forms/ConfirmJobDoneForm.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms.widgets import NumberInput # EmailInput, PasswordInput # 
+from django.forms.widgets import NumberInput  
 
 class ConfirmJobDoneForm(forms.Form): 
     time_to_pay = forms.IntegerField(
@@ -9,3 +9,12 @@ class ConfirmJobDoneForm(forms.Form):
     )
     # Note : django avoid using NumberInput for field having localize property to True
     
+    def post(self, request, *args, **kwargs): 
+        member = None # TODO variable de session
+        
+        # recupere la valeur d'input
+        # verifier que cette valeur est ok
+        
+        member.register_job_done(job_id, helped_one_email) # comment je connais le job ?
+        
+        super(ConfirmJobDoneForm, self).post(request, *args, **kwargs)
