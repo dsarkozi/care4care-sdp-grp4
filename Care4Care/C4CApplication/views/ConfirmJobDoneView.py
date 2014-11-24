6ba254e233d9ed6c7e7f72325fe2563454ef81fd
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-11-25 00:35:59 +0100

    CJDV to be continued...

--- Care4Care/C4CApplication/views/ConfirmJobDoneView.py
+++ Care4Care/C4CApplication/views/ConfirmJobDoneView.py
@@ -6,6 +6,7 @@ class ConfirmJobDoneView(FormView):
     model = Job
     context_object_name = "job"
     template_name = "C4CApplication/ConfirmJobDoneView.html"
+    member = None
 
     def get_object(self):
         
@@ -13,3 +14,13 @@ class ConfirmJobDoneView(FormView):
     
         return job
     
+    def form_valid(self, form):
+        # TODO test if the member has session variables !! -> redirection
+        if ConfirmJobDoneView.member is None:
+            ConfirmJobDoneView.member = Member.objects.get(mail=self.request.session['email'])
+
+        # recupere la valeur d'input
+        # verifier que cette valeur est ok
+        ConfirmJobDoneView.member.register_job_done(job_id, helped_one_email) # comment je connais le job ?
+
+        return super(ConfirmJobDoneView, self).form_valid(form)
--- Care4Care/C4CApplication/views/forms/ConfirmJobDoneForm.py
+++ Care4Care/C4CApplication/views/forms/ConfirmJobDoneForm.py
@@ -9,12 +9,3 @@ class ConfirmJobDoneForm(forms.Form):
     )
     # Note : django avoid using NumberInput for field having localize property to True
     
-    def post(self, request, *args, **kwargs): 
-        member = None # TODO variable de session
-        
-        # recupere la valeur d'input
-        # verifier que cette valeur est ok
-        
-        member.register_job_done(job_id, helped_one_email) # comment je connais le job ?
-        
-        super(ConfirmJobDoneForm, self).post(request, *args, **kwargs)
