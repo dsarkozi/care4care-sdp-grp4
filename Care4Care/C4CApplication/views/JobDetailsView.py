c19757f8f29b5019a05b0d110b77f2c13d6b94ef
Vineuvall <quentin.meyers@student.uclouvain.be>
2014-11-24 18:36:22 +0100

    fin job view

--- Care4Care/C4CApplication/views/JobDetailsView.py
+++ Care4Care/C4CApplication/views/JobDetailsView.py
@@ -1,7 +1,5 @@
 from django.views.generic import DetailView
 from C4CApplication.models.job import Job
-from django import template
-register = template.Library()
 
 class JobDetailsView(DetailView):
     
@@ -14,29 +12,3 @@ class JobDetailsView(DetailView):
         job = super(JobDetailsView, self).get_object()
     
         return job
-    
-    @register.filter
-    def div( value, arg ):
-        '''
-        Divides the value; argument is the divisor.
-        Returns empty string on any error.
-        '''
-        try:
-            value = int( value )
-            arg = int( arg )
-            if arg: return value / arg
-        except: pass
-        return ''
-    
-    @register.tag('mod')
-    def mod( value, arg ):
-        '''
-        Divides the value; argument is the divisor.
-        Returns empty string on any error.
-        '''
-        try:
-            value = int( value )
-            arg = int( arg )
-            if arg: return value % arg
-        except: pass
-        return ''
