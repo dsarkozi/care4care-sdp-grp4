2a2e0a6447715d3fa623a481fe982388c4b966d0
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-11-24 21:40:07 +0100

    continue ConfirmJobDone

--- Care4Care/C4CApplication/views/forms/ConfirmJobDoneForm.py
+++ Care4Care/C4CApplication/views/forms/ConfirmJobDoneForm.py
@@ -4,7 +4,7 @@ from django.forms.widgets import NumberInput # EmailInput, PasswordInput #
 class ConfirmJobDoneForm(forms.Form): 
     time_to_pay = forms.IntegerField(
         widget=NumberInput(
-            attrs={'placeholder': 'Time allowance for the job', 'autofocus': 'true'}
+            attrs={'placeholder': 'Time allowance', 'autofocus': 'true'}
         )
     )
     # Note : django avoid using NumberInput for field having localize property to True
