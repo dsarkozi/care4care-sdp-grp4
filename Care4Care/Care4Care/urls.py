d9c746c217bd374b695b7218c52c2c3ddf2d6fb6
Vineuvall <quentin.meyers@student.uclouvain.be>
2014-11-25 10:26:36 +0100

    ajout password (David tyran !!!) et ajout bouton participate dans jobdetails

--- Care4Care/Care4Care/urls.py
+++ Care4Care/Care4Care/urls.py
@@ -10,6 +10,7 @@ from C4CApplication.views.BranchListView import BranchListView
 from C4CApplication.views.JobDetailsView import JobDetailsView
 from C4CApplication.views.MemberListView import MemberListView
 from C4CApplication.views.ConfirmJobDoneView import ConfirmJobDoneView
+from C4CApplication.views.ParticipateJobRedirectView import ParticipateJobRedirectView
 
 
 urlpatterns = patterns('',
@@ -26,6 +27,7 @@ urlpatterns = patterns('',
 
     url(r'^logout$', LogoutView.as_view()),
     url(r'^jobdetails/(?P<pk>\d+)$', JobDetailsView.as_view()),
+    url(r'^participatejob/(?P<pk>\d+)$', ParticipateJobRedirectView.as_view(), name='participatejob'),
     #url(r'^confirmjobdone/(?P<pk>\d+)$', ConfirmJobDoneView.as_view()),
     url(r'^profile$', 'C4CApplication.views.test.profile'),
 
