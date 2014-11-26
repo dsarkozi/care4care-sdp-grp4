b7f0690cc90574b1cb7bc68b27886208a7e69dd9
Gautier <Gautier@Gautier-PC>
2014-11-26 15:39:30 +0100

    buttons "view received messages" and "view sent messages" in listMessages (mailbox)

--- Care4Care/C4CApplication/views/ListMessages.py
+++ Care4Care/C4CApplication/views/ListMessages.py
@@ -3,6 +3,8 @@ from django.core.exceptions import PermissionDenied
 from django.views.generic import ListView
 
 
+from C4CApplication.views.utils import create_user
+from C4CApplication.meta.user import User
 
 
 class ListMessages(ListView):
@@ -10,4 +12,18 @@ class ListMessages(ListView):
     template_name = "C4CApplication/ListMessages.html"
     paginate_by = 5
     
+    def dispatch(self, request, *args, **kwargs):
+        if 'email' not in self.request.session:
+            raise PermissionDenied  # HTTP 403
+        self.user = create_user(self.request.session['email'])
+        return super(ListMessages, self).dispatch(request, *args, **kwargs)
     
+    def get_context_data(self, **kwargs):
+        context = ListView.get_context_data(self, **kwargs)
+        context['received'] = self.received
+        context['member'] = self.user
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        self.received = kwargs['received']
+        return super(ListMessages, self).get(request, *args, **kwargs)
