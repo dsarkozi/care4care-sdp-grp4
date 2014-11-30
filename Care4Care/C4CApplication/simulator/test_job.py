0fb693df6c5fe2fd9534c8f1b4b0c7972a996ad9
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-11-30 15:59:06 +0100

    test create job offer

--- Care4Care/C4CApplication/simulator/test_job.py
+++ Care4Care/C4CApplication/simulator/test_job.py
@@ -4,6 +4,43 @@ import time
 
 class SeleniumTestJob(MySeleniumTests):
 
+    # test pour creer une demande de job (I need help)
+    def test_offer_job(self):
+        self.populate_db()
+        
+        # log in
+        self.selenium.get('%s%s' % (self.live_server_url, ''))
+        time.sleep(1)
+        username_input = self.selenium.find_element_by_name("email")
+        username_input.send_keys('mathieu.jadin@student.uclouvain.be')
+        time.sleep(1)
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('azertyuiop')
+        time.sleep(1)
+        self.selenium.find_element_by_xpath('//input[@value="Login"]').click()
+        time.sleep(1)
+        
+        print("Logged for create job")
+        
+        # Test create job
+        self.selenium.get('%s%s' % (self.live_server_url, '/newjob/demand'))
+        time.sleep(1)
+        title_input = self.selenium.find_element_by_name("title")
+        title_input.send_keys('New demand test')
+        time.sleep(1)
+        desc_input = self.selenium.find_element_by_name("desc")
+        desc_input.send_keys('Description of a demand test')
+        time.sleep(1)
+        # TODO aller voir select sur internet
+        #categories_input = Select(self.selenium.find_element_by_name("categories"))
+        #categories_input.select_by_visible_text("Visit") 
+        #time.sleep(1)
+        # TODO idem pour 'frequency', 'subfrequency', 'weekdays', 'dayparts', 
+        #'specific' and 'visibility'
+        self.selenium.find_element_by_xpath('//input[@value="Post request"]').click()
+        self.assertEqual(0, 0)
+        return True
+
     def test_register_job_done(self):
         self.populate_db()
         
