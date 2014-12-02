6ebf251ca17dc9026c0d2169c4cbc5bd2c40fa00
Vineuvall <quentin.meyers@student.uclouvain.be>
2014-12-02 11:49:07 +0100

    To pull les modif de guillaume

--- Care4Care/C4CApplication/page_objects/BranchListPage.py
+++ Care4Care/C4CApplication/page_objects/BranchListPage.py
@@ -1,7 +1,10 @@
-from C4CApplication.page_objects.Page import Page
+from C4CApplication.page_objects.FixedPage import FixedPage
 from selenium.webdriver.common.by import By
 
 import time
 
 
-class BranchListPage(Page):
+class BranchListPage(FixedPage):
+    
+    def __init__(self, driver):
+        super().__init__(driver)
