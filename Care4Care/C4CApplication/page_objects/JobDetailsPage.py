4353625eb4a200de0e4437997473fd6302ea823c
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-12-02 11:19:34 +0100

    JobTest à finir plus tard

--- Care4Care/C4CApplication/page_objects/JobDetailsPage.py
+++ Care4Care/C4CApplication/page_objects/JobDetailsPage.py
@@ -11,12 +11,21 @@ class JobDetailsPage(FixedPage):
         super().__init__(driver)
         self.participate_button    = self.driver.find_element_by_xpath('//a[@value="Participate"]')
         try : 
-            self.delete_job_button = self.driver.find_element_by_xpath('//a[@value="Delete job"]')
+            self.delete_job_button = self.driver.find_elements_by_xpath('//a[@value="Delete job"]')
         except : 
             print("No button delete job found !")
             self.delete_job_button = None
-        self.choose_member_button = self.driver.find_element_by_xpath('//a[@value="ChoiceMember"]')
+        # Impossiiiiiiiiiiiible ! That fucking bullshit de merde !!!!!
+        self.choose_member_buttons = self.driver.find_elements(By.PARTIAL_LINK_TEXT, 'Choose')
 
     def click_on_participate(self):
         self.participate_button.click()
+        return self
+    
+    # Impossiiiiiiiiiiiible ! On ne sait pas retrouver choose_member_button
+    def click_on_choose_member(self):
+        print(self.choose_member_buttons)
+        for m_web_el in self.choose_member_buttons:
+            print(m_web_el.text)
+        #self.choose_member_button.click()
         return self
