00f8a08e0b38cb4300a48479375d8988cd34b5d4
jadinm <mathieu.jadin@student.uclouvain.be>
2014-12-02 15:14:40 +0100

    Début de la modif du jobDetailsView

--- Care4Care/C4CApplication/views/JobDetailsView.py
+++ Care4Care/C4CApplication/views/JobDetailsView.py
@@ -2,14 +2,15 @@ from django.views.generic import DetailView
 from django.core.exceptions import PermissionDenied
 from C4CApplication.views.utils import create_user
 from C4CApplication.models.job import Job
+from django.views.generic import FormView
 
-
-class JobDetailsView(DetailView):
+class JobDetailsView(DetailView, FormView):
     
     model = Job
     context_object_name = "job"
     template_name = "C4CApplication/JobDetails.html"
     connected_member = None
+    #TODO form class
     
     def dispatch(self, request, *args, **kwargs):
         # Create the object representing the user
@@ -22,8 +23,13 @@ class JobDetailsView(DetailView):
     def get_context_data(self, **kwargs):
         context = {}
         context = super(JobDetailsView, self).get_context_data(**context)
+        context['form'] = self.form
         context['member'] = self.connected_member.db_member
         return context
+
+    def form_valid(self, form):
+
+        #TODO tratement de la date
 
     def get_object(self):
         
