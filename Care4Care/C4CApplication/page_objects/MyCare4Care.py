e85c7b3fcf9fde83eaac5be0fa87fa4fbbea6a73
Vineuvall <quentin.meyers@student.uclouvain.be>
2014-12-03 16:49:20 +0100

    New DB + Gros changement job details

--- Care4Care/C4CApplication/page_objects/MyCare4Care.py
+++ Care4Care/C4CApplication/page_objects/MyCare4Care.py
@@ -1,5 +1,6 @@
 from C4CApplication.page_objects.FixedPage import FixedPage
 #from C4CApplication.page_objects.HomePage import HomePage
+from C4CApplication.page_objects.CreateBranchView import CreateBranchView
 from selenium.webdriver.common.by import By
 
 
@@ -8,6 +9,12 @@ class MyCare4Care(FixedPage):
     def __init__(self, driver):
         super().__init__(driver)
         self.logout_button = self.driver.find_element(By.XPATH, '//a[@value="Logout"]')
+        self.new_branch_button = None
+    
+    def BP_click_on_new_branch(self):
+        self.new_branch_button = self.driver.find_element(By.XPATH, '//a[@value="new_branch"]')
+        self.new_branch_button.click()
+        return CreateBranchView(self.driver)
     
     def give_time(self):
         #give_time_button = self.driver.find_element(By.XPATH, )
