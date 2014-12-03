edff20a50144f8e363662dc739233b5b8483b747
jadinm <mathieu.jadin@student.uclouvain.be>
2014-12-03 16:36:27 +0100

    Coquille avec les statut réglée

--- Care4Care/C4CApplication/views/forms/ModifProfileForm.py
+++ Care4Care/C4CApplication/views/forms/ModifProfileForm.py
@@ -10,9 +10,9 @@ class ModifProfileForm(forms.Form):
     street  = forms.CharField(max_length=100, widget=forms.TextInput(  attrs={'placeholder': 'street', 'size' : 32}))
     zip     = forms.CharField(max_length=100, widget=forms.NumberInput(attrs={'placeholder': 'zip',    'size' : 10}))
     town    = forms.CharField(max_length=100, widget=forms.TextInput(  attrs={'placeholder': 'town',   'size' : 32}))
-    picture = forms.ImageField()
     
     #infos facultatives
+    picture = forms.ImageField(required=False)
     fixed_phone  = forms.CharField(max_length=100, required=False, widget=forms.NumberInput(attrs={'placeholder': 'fixed phone',' size' : 88  }))
     mobile_phone = forms.CharField(max_length=100, required=False, widget=forms.NumberInput(attrs={'placeholder': 'mobile phone',' size' : 10}))
 
