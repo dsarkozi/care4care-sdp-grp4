77f6f35f1ea90b9fda7c11c8db3c5432b08a1140
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-12-03 10:42:42 +0100

    Begin ProfileTest

--- Care4Care/C4CApplication/page_objects/ProfilePage.py
+++ Care4Care/C4CApplication/page_objects/ProfilePage.py
@@ -3,3 +3,12 @@ from selenium.webdriver.common.by import By
 
 
 class ProfilePage(MyCare4CarePage):
+    self.browse_button = None
+    self.delete_pic_button = None
+    self.street_input = None
+    self.number_input = None
+    self.postal_code_input = None
+    self.town_input = None
+    self.phone_num_input = None
+    self.mobile_phone_input = None
+    
--- Care4Care/C4CApplication/simulator/ProfileTest.py
+++ Care4Care/C4CApplication/simulator/ProfileTest.py
@@ -1,4 +1,5 @@
 from C4CApplication.simulator.super_class import MySeleniumTests
+from C4CApplication.page_objects.HomePage import HomePage
 
 
 import time
@@ -6,26 +7,37 @@ import time
 
 class ProfileTest(MySeleniumTests):
 
-    def see_data_test(self):
+    """def see_data_test(self):
         self.populate_db()
         
         self.selenium.get('%s%s' % (self.live_server_url, ''))
+        
+        self.assertEqual(0, 0)
+        return True"""
+    
+    def test_update_data(self):
+        self.populate_db()
+        
+        # log in
+        self.selenium.get('%s%s' % (self.live_server_url, ''))
+        username_input = self.selenium.find_element_by_name("email")
+        username_input.send_keys('mathieu.jadin@student.uclouvain.be')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('azertyuiop')
+        self.selenium.find_element_by_xpath('//input[@value="Login"]').click()
+        
+        self.selenium.get('%s%s' % (self.live_server_url, '/modifprofile/'))
+        time.sleep(2)
+        
+        
         
         self.assertEqual(0, 0)
         return True
     
-    def update_data_test(self):
+    """def confidentiality_test(self):
         self.populate_db()
         
         self.selenium.get('%s%s' % (self.live_server_url, ''))
         
         self.assertEqual(0, 0)
-        return True
-    
-    def confidentiality_test(self):
-        self.populate_db()
-        
-        self.selenium.get('%s%s' % (self.live_server_url, ''))
-        
-        self.assertEqual(0, 0)
-        return True
+        return True"""
