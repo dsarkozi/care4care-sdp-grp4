1601070e6f13937693abfa00a743b7e908e5fa2b
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-12-03 21:24:02 +0100

    MessageTest presque fini

--- Care4Care/C4CApplication/page_objects/ListMessagesPage.py
+++ Care4Care/C4CApplication/page_objects/ListMessagesPage.py
@@ -10,7 +10,7 @@ class ListMessagesPage(FixedPage):
         
         self.switch_button = None
         self.new_message_button = None
-        self.read_more_buttons = None
+        self.read_more_buttons = []
         
         for el in buttons: # maybe change this if the language change
             print(el.text)
@@ -19,6 +19,23 @@ class ListMessagesPage(FixedPage):
             elif el.text.startswith('Write') :
                 self.new_message_button = el
             elif el.text.startswith('Read') :
-                self.read_more_buttons = el
+                self.read_more_buttons.append(el)
         #self.new_message_button = self.driver.find_element_by_xpath('//a[@value="new_message"]') 
         
+    def click_on_read_more(self, num):
+        if len(self.read_more_buttons) < num+1:
+            print("No such a message : \nAre you sure that at least "+str((num+1))+" messages have been received ?")
+            return self
+        
+        self.read_more_buttons[num].click()
+        return self
+    
+    def click_on_new_message(self):
+        self.new_message_button.click()
+        return self
+    
+    def click_on_switch(self):
+        if self.switch_button is not None: 
+            self.switch_button.click()
+        else: print("Switch button not found !")
+        return ListMessagesPage(self.driver)
--- Care4Care/C4CApplication/simulator/MessageTest.py
+++ Care4Care/C4CApplication/simulator/MessageTest.py
@@ -6,7 +6,7 @@ import time
 
 class MessageTest(MySeleniumTests):
 
-    def test_see_message(self):
+    def test_see_message_received(self):
         self.populate_db()
         
         # log in
@@ -20,14 +20,39 @@ class MessageTest(MySeleniumTests):
         self.selenium.get('%s%s' % (self.live_server_url, '/list_messages/1'))
         page = ListMessagesPage(self.selenium)
         
+        page = page.click_on_read_more(0)
+        time.sleep(5)
         
         self.assertEqual(0, 0)
         return True
     
-    """def send_message_test(self):
+    def test_see_message_sent(self):
+        self.populate_db()
+        
+        # log in
+        self.selenium.get('%s%s' % (self.live_server_url, ''))
+        username_input = self.selenium.find_element_by_name("email")
+        username_input.send_keys('kim.mens@gmail.com')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('azertyuiop')
+        self.selenium.find_element_by_xpath('//input[@value="Login"]').click()
+        
+        self.selenium.get('%s%s' % (self.live_server_url, '/list_messages/1'))
+        page = ListMessagesPage(self.selenium)
+        
+        page = page.click_on_switch()
+        time.sleep(1)
+        
+        page = page.click_on_read_more(0)
+        time.sleep(1)
+        
+        self.assertEqual(0, 0)
+        return True
+    
+    def send_message_test(self):
         self.populate_db()
         
         self.selenium.get('%s%s' % (self.live_server_url, ''))
         
         self.assertEqual(0, 0)
-        return True"""
+        return True
