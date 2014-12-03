f084697631b8e51d16d9c58eb3147c1f51000868
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-12-03 17:44:34 +0100

    To pull

--- Care4Care/C4CApplication/page_objects/ListMessagesPage.py
+++ Care4Care/C4CApplication/page_objects/ListMessagesPage.py
@@ -3,3 +3,9 @@ from selenium.webdriver.common.by import By
 
 
 class ListMessagesPage(MyCare4CarePage):
+    
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.switch_button = None
+        self.new_message_button = None 
+        self.read_more_buttons = None
