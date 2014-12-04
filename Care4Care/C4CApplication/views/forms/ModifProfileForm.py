379c4f642175fb35a7b3dcfb1ef3bcbad429e3ec
vladarS <Vladar@wifi-student1-669.sri.ucl.ac.be>
2014-12-04 19:29:38 +0100

    password verification

--- Care4Care/C4CApplication/views/forms/ModifProfileForm.py
+++ Care4Care/C4CApplication/views/forms/ModifProfileForm.py
@@ -5,8 +5,8 @@ from C4CApplication.models.member import Member
 
 class ModifProfileForm(forms.Form):
 
-    new_password   = forms.CharField( max_length=100 , required=False , widget=forms.PasswordInput(attrs={'placeholder': 'new_password', 'size' : 44}))
-    #password   = forms.CharField( max_length=100 , required=False  , widget=forms.PasswordInput(attrs={'placeholder': 'password', 'size' : 44}))
+    new_password   = forms.CharField( max_length=100 , required=False , widget=forms.PasswordInput(attrs={'placeholder': 'new password', 'size' : 44}))
+    password   = forms.CharField( max_length=100 , required=False  , widget=forms.PasswordInput(attrs={'placeholder': 'password verification', 'size' : 44}))
 
     #adresse
     street  = forms.CharField(max_length=100, widget=forms.TextInput(  attrs={'placeholder': 'street, number, more information', 'size' : 32}))
@@ -22,15 +22,17 @@ class ModifProfileForm(forms.Form):
         cleaned_data = super(ModifProfileForm, self).clean()
         new_password = ''  
         new_password = cleaned_data.get('new_password')
-        #password = ''
-        #password = cleaned_data.get('new_password')
+        password = ''
+        password = cleaned_data.get('password')
         
         #mot de passe
         if new_password:
             if len(new_password) <8 and len(new_password) !=0:
                 msg = "Your password is too short, it should be at least 8 characters long."
                 self.add_error("new_password",msg)
-                
+            if new_password!=password:
+                msg = "You entered different passwords"
+                self.add_error("new_password",msg)
         
 
         #address: verifier qu'il n'y a pas de , dans les champs de l'address
