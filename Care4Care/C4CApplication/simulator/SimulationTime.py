132d1f36844ca0ae3d99e5d93890571c3a6b1d3d
jadinm <mathieu.jadin@student.uclouvain.be>
2014-12-05 10:26:45 +0100

    Correction du temps de simulation

--- Care4Care/C4CApplication/simulator/SimulationTime.py
+++ Care4Care/C4CApplication/simulator/SimulationTime.py
@@ -3,19 +3,26 @@ from django.contrib.sessions.models import Session
 from django.conf import settings
 
 
-def change_server_time(time):
+class SimulationTime:
 
-    #Create a new session
-    s = SessionStore(session_key='')
-    s.save()
-    old_s = Session.objects.get(session_key=s.session_key)
+    session = None
 
-    #Copy it to a column with an id fixed
-    new_session = Session(session_key=settings.SESSION_ID_SIMU, session_data=old_s.session_data,
-                          expire_date=old_s.expire_date)
-    new_session.save()
+    @staticmethod
+    def change_server_time(time):
 
-    #Change the current date
-    session_dictionary = SessionStore(session_key=settings.SESSION_ID_SIMU)
-    session_dictionary['current_date'] = time
-    session_dictionary.save()
+        if SimulationTime.session is None:
+            #Create a new session
+            s = SessionStore(session_key='')
+            s.save()
+            old_s = Session.objects.get(session_key=s.session_key)
+
+            #Copy it to a column with an id fixed
+            new_session = Session(session_key=settings.SESSION_ID_SIMU, session_data=old_s.session_data,
+                                  expire_date=old_s.expire_date)
+            new_session.save()
+            #Change the current date
+            session_dictionary = SessionStore(session_key=settings.SESSION_ID_SIMU)
+            SimulationTime.session = session_dictionary
+
+        SimulationTime.session['current_date'] = time
+        SimulationTime.session.save()
