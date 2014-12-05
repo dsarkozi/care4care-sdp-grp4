0edbce81805723487d605fcaf10aef19e817031e
Vineuvall <quentin.meyers@student.uclouvain.be>
2014-12-05 16:37:01 +0100

    to pull

--- Care4Care/C4CApplication/views/ProfileView.py
+++ Care4Care/C4CApplication/views/ProfileView.py
@@ -9,6 +9,22 @@ class ProfileView(DetailView):
     context_object_name = "member_shown"
     model = Member
     user = None
+    
+    TAG_REVERSE = {
+        1         : 'Non member',               #000001
+        2         : 'Member',                   #000010
+        4         : 'Verified',                 #000100
+        8         : 'Volunteer',                #001000
+        16        : 'Branch officer',           #010000
+        32        : 'BP admin',                 #100000
+    }
+    
+    def get_tag_member(db_member):
+        tag = ""
+        for key, value in ProfileView.TAG_REVERSE.items():
+            if key & db_member.tag == key:
+                tag += "%s & " % (value)
+        return tag[:-3]
 
     def dispatch(self, request, *args, **kwargs):
         if 'email' not in self.request.session:
@@ -19,6 +35,7 @@ class ProfileView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(ProfileView, self).get_context_data(**kwargs)
         context['member'] = self.user.db_member
+        context['tag_member'] = ProfileView.get_tag_member(context['member_shown'])
         context['connected'] = 'email' in self.request.session
         return context
 
