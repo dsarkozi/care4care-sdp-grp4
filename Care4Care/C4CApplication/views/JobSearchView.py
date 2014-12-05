82ee2657c3f4d7649c567558a4907ded7d373115
David Sarkozi <david.sarkozi@student.uclouvain.be>
2014-12-05 17:03:39 +0100

    JobSearch init

---
+++ Care4Care/C4CApplication/views/JobSearchView.py
@@ -0,0 +1,10 @@
+from django.views.generic.list import ListView
+from C4CApplication.models.job import Job
+
+
+class JobSearchView(ListView):
+    queryset = Job.objects.all()
+    model = Job
+    paginate_by = 10
+    context_object_name = 'jobs'
+    template_name = 'C4CApplication/JobSearch.html'
