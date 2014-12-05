eb3704dc525009fec88f0dc6605954a4401d1278
gmoyson <guillaume@guillaume-HP-Mini-110-3500>
2014-12-05 09:46:08 +0100

    BranchOff simulation to be continued...

--- Care4Care/C4CApplication/simulation/BranchOffSimulation.py
+++ Care4Care/C4CApplication/simulation/BranchOffSimulation.py
@@ -1,6 +1,8 @@
 from C4CApplication.unit_tests.super_class import MySeleniumTests
 from C4CApplication.page_objects.HomePage import HomePage
 from C4CApplication.page_objects.BranchListPage import BranchListPage
+from C4CApplication.page_objects.MyCare4Care import MyCare4Care
+
 
 import time
 
@@ -26,5 +28,16 @@ class BranchOffSimulation(MySeleniumTests):
         time.sleep(1)
         page = page.click_on_log_as_member() # log as the member 
         time.sleep(1)
+        
+        page = MyCare4Care(self.selenium)
+        #page = page.click_on_i_need_help() # create help demand
+        time.sleep(1)
+        
+        # reprendre etapes de helpedsimulation
+        
+        page = page.click_home()
+        page = HomePage(self.selenium)
+        page.click_on_logout()
+        print("FIN de la simulation !")
                 
         return True
