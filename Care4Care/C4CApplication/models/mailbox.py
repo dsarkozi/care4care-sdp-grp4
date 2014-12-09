ba5fab1bf588cf669c143c8a37bb9a91c9d816e7
GuillaumeG <guillaume.gustin@student.uclouvain.be>
2014-12-09 14:52:29 +0100

    AccountsStats/Mail

--- Care4Care/C4CApplication/models/mailbox.py
+++ Care4Care/C4CApplication/models/mailbox.py
@@ -14,4 +14,5 @@ class Mailbox(models.Model):
         for mailbox in list_mailbox :
             if not mailbox.status :
                 has_mail_not_readed += 1
+        print("TRUC = ",has_mail_not_readed)
         return has_mail_not_readed
