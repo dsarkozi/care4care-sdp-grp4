c1cf85b2a5d54afcc110d127a4f32c6a426242f9
David Sarkozi <david.sarkozi@student.uclouvain.be>
2014-12-11 04:54:24 +0100

    SearchJob translation

--- Care4Care/C4CApplication/views/forms/SearchForm.py
+++ Care4Care/C4CApplication/views/forms/SearchForm.py
@@ -1,7 +1,6 @@
 from django import forms
-from django.forms.widgets import TextInput, EmailInput
-
-from C4CApplication.models.job import Job
+from django.forms.widgets import EmailInput
+from django.utils.translation import ugettext_lazy as _
 
 
 class SearchForm(forms.Form):
@@ -9,10 +8,10 @@ class SearchForm(forms.Form):
     email_member = forms.EmailField(
         widget=EmailInput(
             attrs={'autofocus':'true', 'placeholder':'member@mail.com'}
-        ), label="Filter the jobs created by the member", required=False
+        ), label=_("Filter the jobs created by the member"), required=False
     )
 
     type_of_job = forms.ChoiceField(
         widget=forms.RadioSelect,
-        choices=(('offer', 'See help offers'), ('demand', 'See help demands'))
+        choices=(('offer', _('See help offers')), ('demand', _('See help demands')))
     )
